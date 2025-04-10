28e5ac673dd863f30d102172b10273f8c99f60e9
Nazisagit <pgnazrin@gmail.com>
2025-04-10 12:19:11 +0800

    initialized wallet model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_09_084217) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_09_142843) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -31,5 +31,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_09_084217) do
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
+  create_table "wallets", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_wallets_on_user_id"
+  end
+
   add_foreign_key "sessions", "users"
+  add_foreign_key "wallets", "users"
 end
---
+++ spec/models/wallet_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Wallet, type: :model do
+  describe "assocations" do
+    it { should belong_to(:user) }
+  end
+end
