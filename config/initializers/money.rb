818cb9cdd90d3cdd1cda37c7e0e52dff5d174a76
Nazisagit <pgnazrin@gmail.com>
2025-04-10 18:23:18 +0800

    configured money localisation, and usd as default currency

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,10 +1,9 @@
 # encoding : utf-8
 
 MoneyRails.configure do |config|
-
   # To set the default currency
   #
-  # config.default_currency = :usd
+  config.default_currency = :usd
 
   # Set default bank object
   #
@@ -85,7 +84,7 @@ MoneyRails.configure do |config|
 
   # If you would like to use I18n localization (formatting depends on the
   # locale):
-  # config.locale_backend = :i18n
+  config.locale_backend = :i18n
   #
   # Example (using default localization from rails-i18n):
   #
