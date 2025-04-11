f3c5b3cdb0535124aa38c69a3637b724717891cc
Nazisagit <pgnazrin@gmail.com>
2025-04-11 11:16:24 +0800

    updated annotation

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -9,6 +9,15 @@
 #  updated_at         :datetime         not null
 #  receiver_wallet_id :bigint
 #  sender_wallet_id   :bigint
+#  wallet_id          :bigint           not null
+#
+# Indexes
+#
+#  index_transactions_on_wallet_id  (wallet_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (wallet_id => wallets.id)
 #
 class Transfer < Transaction
   belongs_to :sender_wallet, class_name: "Wallet"
