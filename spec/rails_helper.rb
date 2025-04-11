fce5f67b2a430e6aeef6fcc33de3c4d1137c5e18
Nazisagit <pgnazrin@gmail.com>
2025-04-11 11:15:14 +0800

    added a helper to parse json for api tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'support/api_helper.rb'
 require 'support/factory_bot.rb'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
@@ -68,6 +69,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include ApiHelper
 end
 
 Shoulda::Matchers.configure do |config|
