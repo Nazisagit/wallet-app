329ce75deb9a13e48b1d80d3095315c22731d327
Nazisagit <pgnazrin@gmail.com>
2025-04-13 03:05:17 +0800

    added specs to test user callbacks

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe User, type: :model do
     it { should have_many(:sessions) }
     it { should have_one(:wallet) }
   end
+
+  describe "callbacks" do
+    context "after_create" do
+      let(:user) { build(:user) }
+
+      it { expect { user.save }.to change(Wallet, :count).by(1) }
+      it do
+        expect(user.wallet).to be_nil
+        user.save
+        expect(user.wallet).to be_an_instance_of(Wallet)
+      end
+    end
+  end
 end
