2a1a7aa2bf7ef6337cda25bf3294c658b80ab854
Nazisagit <pgnazrin@gmail.com>
2025-04-13 01:29:15 +0800

    added secure token for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id              :bigint           not null, primary key
+#  auth_token      :string
 #  email_address   :string           not null
 #  password_digest :string           not null
 #  created_at      :datetime         not null
@@ -10,10 +11,12 @@
 #
 # Indexes
 #
+#  index_users_on_auth_token     (auth_token) UNIQUE
 #  index_users_on_email_address  (email_address) UNIQUE
 #
 class User < ApplicationRecord
   has_secure_password
+  has_secure_token :auth_token
   has_many :sessions, dependent: :destroy
   has_one :wallet, dependent: :destroy
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_12_062836) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_12_144656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -40,6 +40,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_12_062836) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "auth_token"
+    t.index ["auth_token"], name: "index_users_on_auth_token", unique: true
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
